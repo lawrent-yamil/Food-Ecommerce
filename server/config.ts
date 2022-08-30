05ffff40f909fc6b1de9fe8e0554a24d901aba4f
McFly <kencamacho19@gmail.com>
2022-08-30 09:31:17 -0600

    xdddd

--- server/config.ts
+++ server/config.ts
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config();
 console.log("PUTA")
+console.log("PUTA")
+console.log("PUTA")
+console.log("PUTA")
+console.log("PUTA")
+
 console.log("Acepteme la pr solo es un console.log xd")
 
 export const PORT = process.env?.PORT || 5000
